5063a43cbf29ef1d4d456c9cdd32947df6127eb4
seunghyeokleeme <seunghyeok.tech@gmail.com>
2019-12-24 16:42:37 +0900

    3.1 ~ 3.4

---
+++ es6/3.js
@@ -0,0 +1,19 @@
+// let currentTempC = 22; // 섭씨온도
+// // 변수 선언(생성) & 초깃값 할당
+// currentTempC = 22.5;
+// // 값 변경
+
+// let targetTempC; // let targetTempC = undifined; 와 같습니다.
+
+// let targetTempC,
+//   room1 = "conference_room_a",
+//   room2 = "lobby";
+
+// const ROOM_TEMP_C = 21.5,
+//   MAX_TEMP_C = 30;
+
+let room1 = "conference_room_a"; // "conference_room_a" (따옴표 안)은 리터럴입니다.
+
+let currentRoom = room1; // 이제 currentRoom의 값은 room 1의 값("conference_room_a")과 같습니다.
+
+currentRoom = conference_room_a; // 에러가 일어납니다. conference_room_a란 식별자가 존재하지 않습니다.
