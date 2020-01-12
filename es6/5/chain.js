ab539f1d46e02e2c25481e683662b681b1c77068
seunghyeokleeme <seunghyeok.tech@gmail.com>
2020-01-12 17:30:58 +0900

    5

---
+++ es6/5/chain.js
@@ -0,0 +1,8 @@
+const nums = [3, 5, 15, 7, 5];
+let n,
+  i = 0;
+while ((n = nums[i]) < 10 && i++ < nums.length) {
+  console.log(`Number less than 10 found: ${n}.`);
+}
+console.log(`Number greater than 10 found: ${n}.`);
+console.log(`${nums.length - i - 1} numbers remain.`);
